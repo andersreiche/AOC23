32d7ccbd4f1d10b92a2c13f761cf9a41f1ae3130
Anders Reiche <andersreiche@gmail.com>
2023-12-13 23:30:55 +0100

    minified and ooptimized

--- Day2/d2p1.cpp
+++ Day2/d2p1.cpp
@@ -11,71 +11,50 @@ const int maxBlue = 14;
 
 int run() {
   std::ifstream file("puzzleInput.txt");
-
   std::string line;
   int sum = 0;
 
-  // GAMES in the file
-  while (std::getline(file, line)) {
+  while (std::getline(file, line)) { // GAMES in the file
+      bool validGame = true;
       size_t idStartPos = line.find(' ');
       size_t idEndPos = line.find(':');
       std::string gameIdStr = line.substr(idStartPos, idEndPos - idStartPos);
       int gameId = std::stoi(gameIdStr);
-      bool validGame = true;
       size_t startSetPos = idEndPos + 2;
       size_t endSetPos = line.find(';', startSetPos);
       std::string set = line.substr(startSetPos, endSetPos - startSetPos);
       size_t startPairPos = 0;
-
-      // SETS in the game
-      while (endSetPos != std::string::npos) {
-        size_t endPairPos = set.find_first_of(';,');
-
-        // PAIRS in the set
-        while (startPairPos < set.length()) {
-            size_t numberStartPos = startPairPos;
-            size_t numberEndPos = set.find(' ', numberStartPos);
-            int amount = std::stoi(set.substr(numberStartPos, numberEndPos - numberStartPos));
-            size_t colorStartPos =  numberEndPos + 1;
-            size_t colorEndPos = set.find_first_not_of("bluegrdn", colorStartPos); // letters in blue, green, red.
-            std::string color = set.substr(colorStartPos, colorEndPos - colorStartPos);
-            std::string pair = set.substr(numberStartPos, colorEndPos - numberStartPos);
-            // // Check if amount exceeds the max for the color
-            if ((color == "red" && amount > maxRed) ||
-                (color == "green" && amount > maxGreen) ||
-                (color == "blue" && amount > maxBlue)) {
-                validGame = false;
-                break;
-            }
-            if (endPairPos == std::string::npos) {
-              startPairPos = 0;
-              break; // This was the last pair in the set
-            }
-            startPairPos = endPairPos + 2; // skip the , or ; and the space
-            endPairPos = set.find_first_of(';,', startPairPos);
+      while (true) { // SETS in the game
+          size_t endPairPos = set.find_first_of(';,');
+          while (true) { // PAIRS in the set
+                size_t numberStartPos = startPairPos;
+                size_t numberEndPos = set.find(' ', numberStartPos);
+                int amount = std::stoi(set.substr(numberStartPos, numberEndPos - numberStartPos));
+                size_t colorStartPos =  numberEndPos + 1;
+                size_t colorEndPos = set.find_first_not_of("bluegrdn", colorStartPos); // letters in blue, green, red.
+                std::string color = set.substr(colorStartPos, colorEndPos - colorStartPos);
+                std::string pair = set.substr(numberStartPos, colorEndPos - numberStartPos);
+                if ((color == "red" && amount > maxRed) || (color == "green" && amount > maxGreen) || (color == "blue" && amount > maxBlue)) {
+                    validGame = false;
+                    break;
+                }
+                if (endPairPos == std::string::npos) {
+                  startPairPos = 0;
+                  break; // This was the last pair in the set
+                }
+                startPairPos = endPairPos + 2; // skip the , or ; and the space
+                endPairPos = set.find_first_of(';,', startPairPos);
+          }
+          if (!validGame) break;
+          startSetPos = endSetPos + 2; // skip semicolon and space
+          if (startSetPos >= line.length()) break;
+          endSetPos = line.find(';', startSetPos);
+          endSetPos = (endSetPos == std::string::npos) ? line.length() : endSetPos;
+          set = line.substr(startSetPos, endSetPos - startSetPos);
         }
-
-        if (!validGame) {
-          break;
-        }
-
-        startSetPos = endSetPos + 2; // skip semicolon and space
-        if (startSetPos >= line.length()) {
-          break;
-        }
-
-        endSetPos = line.find(';', startSetPos);
-        if (endSetPos == std::string::npos) {
-            endSetPos = line.length();
-        }
-        set = line.substr(startSetPos, endSetPos - startSetPos);
-      }
-
-      if (validGame) {
-        sum += gameId;
-      }
-  }
-  return sum;
+        if (validGame) sum += gameId;
+    }
+    return sum;
 }
 
 int main() {
--- Day2/d2p2.cpp
+++ Day2/d2p2.cpp
@@ -6,13 +6,11 @@
 using namespace std::chrono;
 
 int run() {
-  std::ifstream file("puzzleInput.txt");
-
+    std::ifstream file("puzzleInput.txt");
     std::string line;
     uint64_t  sum = 0;
 
-    // GAMES in the file
-    while (std::getline(file, line)) {
+    while (std::getline(file, line)) { // GAMES in the file
         int minRed = 0;
         int minGreen = 0;
         int minBlue = 0;
@@ -20,56 +18,38 @@ int run() {
         size_t idEndPos = line.find(':');
         std::string gameIdStr = line.substr(idStartPos, idEndPos - idStartPos);
         int gameId = std::stoi(gameIdStr);
-        bool validGame = true;
         size_t startSetPos = idEndPos + 2;
         size_t endSetPos = line.find(';', startSetPos);
         std::string set = line.substr(startSetPos, endSetPos - startSetPos);
         size_t startPairPos = 0;
+        while (true) {  // SETS in the game
+            size_t endPairPos = set.find_first_of(';,');
+            while (true) { // PAIRS in the set
+                  size_t numberStartPos = startPairPos;
+                  size_t numberEndPos = set.find(' ', numberStartPos);
+                  int amount = std::stoi(set.substr(numberStartPos, numberEndPos - numberStartPos));
+                  size_t colorStartPos =  numberEndPos + 1;
+                  size_t colorEndPos = set.find_first_not_of("bluegrdn", colorStartPos);
+                  std::string color = set.substr(colorStartPos, colorEndPos - colorStartPos);
+                  std::string pair = set.substr(numberStartPos, colorEndPos - numberStartPos);
 
-        // SETS in the game
-        while (endSetPos != std::string::npos) {
-          size_t endPairPos = set.find_first_of(';,');
-
-          // PAIRS in the set
-          while (startPairPos < set.length()) {
-              size_t numberStartPos = startPairPos;
-              size_t numberEndPos = set.find(' ', numberStartPos);
-              int amount = std::stoi(set.substr(numberStartPos, numberEndPos - numberStartPos));
-              size_t colorStartPos =  numberEndPos + 1;
-              size_t colorEndPos = set.find_first_not_of("bluegrdn", colorStartPos);
-              std::string color = set.substr(colorStartPos, colorEndPos - colorStartPos);
-              std::string pair = set.substr(numberStartPos, colorEndPos - numberStartPos);
-              // Check if amount is new min.
-              if (color == "red" && amount > minRed) {
-                minRed = amount;
-              } 
-              else if (color == "green" && amount > minGreen) {
-                minGreen = amount;
-              } 
-              else if (color == "blue" && amount > minBlue) {
-                minBlue = amount;
-              }
-              if (endPairPos == std::string::npos) {
-                startPairPos = 0;
-                break; // This was the last pair in the set
-              }
-              startPairPos = endPairPos + 2; // skip the , or ; and the space
-              endPairPos = set.find_first_of(';,', startPairPos);
+                  if (color == "red" && amount > minRed)          { minRed = amount; } 
+                  else if (color == "green" && amount > minGreen) { minGreen = amount; } 
+                  else if (color == "blue" && amount > minBlue)   { minBlue = amount; }
+                  if (endPairPos == std::string::npos) {
+                    startPairPos = 0; // reset position for next set
+                    break; // This was the last pair in the set
+                  }
+                  startPairPos = endPairPos + 2; // skip the , or ; and the space
+                  endPairPos = set.find_first_of(';,', startPairPos);
+            }
+            startSetPos = endSetPos + 2; // skip semicolon and space
+            if (startSetPos >= line.length()) break;
+            endSetPos = line.find(';', startSetPos);
+            endSetPos = (endSetPos == std::string::npos) ? line.length() : endSetPos;
+            set = line.substr(startSetPos, endSetPos - startSetPos);
           }
-
-          startSetPos = endSetPos + 2; // skip semicolon and space
-          if (startSetPos >= line.length()) {
-            break;
-          }
-
-          endSetPos = line.find(';', startSetPos);
-          if (endSetPos == std::string::npos) {
-              endSetPos = line.length();
-          }
-          set = line.substr(startSetPos, endSetPos - startSetPos);
-        }
-
-        sum += minRed * minGreen * minBlue;
+          sum += minRed * minGreen * minBlue;
     }
     return sum;
 }
@@ -81,4 +61,4 @@ int main() {
     auto duration = duration_cast<microseconds>(stop - start);
     std::cout << "sum: " << sum << std::endl;
     std::cout << "Code executed in: " << duration.count() << " microseconds." << std::endl;
-  }
+}
